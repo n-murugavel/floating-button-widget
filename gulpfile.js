d8266e53a3cdfb5004f334635e1346d76ad7380b
Murugavel N <murugavel.n@sella.it>
2017-07-06 20:09:39 +0530

    button style, aligntment completed, gulp build done, open feedback form - in progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,76 @@
 var gulp = require('gulp');
+var uglifyCss = require('gulp-clean-css');
+var uglifyJs = require('gulp-uglify');
+var pump = require('pump'); // useful for error handling with gulp-uglify
 var es = require('event-stream');
 var del = require('del');
+var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
 gulp.task('clean-vendors', function(cb) {
 	return del(['css/vendors', 'js/vendors'], {
 		force: true
 	}, cb);
 });
-gulp.task('copy-vendors', ['clean-vendors'], function() {
-	return es.concat(
-		gulp.src('node_modules/jquery/dist/jquery.min.js')
-			.pipe(gulp.dest('js/vendors/jquery')),
+
+gulp.task('clean-build', function(cb) {
+	return del(['dist'], {
+		force: true
+	}, cb);
+});
+
+gulp.task('copy-vendors', function() {
+	return es.merge(
+		/*gulp.src('node_modules/jquery/dist/jquery.min.js')
+			.pipe(gulp.dest('js/vendors/jquery')),*/
 		gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
 			.pipe(gulp.dest('css/vendors/font-awesome/css')),
 		gulp.src('node_modules/font-awesome/fonts/**.*')
-			.pipe(gulp.dest('css/vendors/font-awesome/fonts'))
+			.pipe(gulp.dest('dist/fonts'))
 	);
+});
+
+gulp.task('minify-css', function(cb) {
+	pump(
+		[
+			gulp.src(['css/vendors/font-awesome/css/font-awesome.min.css', 'css/widget-app.css']),
+			concat('widget-app.min.css'),
+			gulp.dest('dist/css'),
+			uglifyCss(),
+			gulp.dest('dist/css')
+		],
+		cb
+	);
+});
+
+gulp.task('minify-js', function(cb) {
+	pump(
+		[
+			gulp.src('js/widget-app.js'),
+			concat('widget-app.js'),
+			rename('widget-app.min.js'),
+			uglifyJs(),
+			gulp.dest('dist/js')
+		],
+		cb
+	);
+});
+
+gulp.task('build', function() {
+	runSequence('clean-vendors', 'clean-build', 'copy-vendors', 'minify-css', 'minify-js', function() {
+		console.log('build - done!');
+	});
+});
+
+gulp.task('clean-for-build', function() {
+	runSequence('clean-vendors', 'clean-build', function() {
+		console.log('clean for build - done!');
+	});
+});
+
+gulp.task('build-app', function() {
+	return runSequence('copy-vendors', 'minify-css', 'minify-js', function() {
+		console.log('build app - done!');
+	});
 });
--- js/widget-app.js
+++ js/widget-app.js
@@ -1,42 +1,129 @@
 (function() {
-	var floatWidget = {
+	var __w = window, __d = document;
+	var ffWidget = {
+		userAgent: navigator.userAgent,
 		noop: function() {},
-		jqueryLocation: 'js/vendors/jquery.min.js',
 		init: function(opts) {
-			if( ! window.jQuery) {
-				this.loadScript(this.jqueryLocation, this.init);
-			} else {
-				this.initEvents();
-			}
+			__d.head.appendChild(
+				this.generateElement('link', [
+					['type', 'text/css'],
+					['rel', 'stylesheet'],
+					['media', 'all'],
+					['href', this.getAppLocation() + '/dist/css/widget-app.min.css']
+				])
+			);
+			this.injectTemplate();
+			this.bindEvents();
 		},
-		initEvents: function() {
-			/*$('.ffw-container').on('mouseenter', '.ffw-button', function() {
-				$('.ffw-operations').addClass('active');
-			}).on('mouseleave', '.ffw-button', function() {
-				$('.ffw-operations').removeClass('active');
-			});*/
+		injectTemplate: function() {
+			__d.body.appendChild(this.getWidgetTemplate());
 		},
-		loadScript: function(url, callback) {
-			if(url) {
-				var cb = callback || this.noop;
-				var scriptElem = document.createElement('script');
-				scriptElem.createAttribute('type', 'text/javascript');
-				scriptElem.createAttribute('src', url);
-				if(scriptElem.addEventListener) {
-					scriptElem.addEventListener('load', cb, false);
-				} else if(scriptElem.readyState) {
-					scriptElem.onreadystatechange = cb;
-				}
-				document.getElementsByTagName('head')[0].appendChild(scriptElem);
-			}
+		bindEvents: function() {
+			__d.querySelector('.ffw-feedback').addEventListener('click', function() {
+				this.openFeedbackForm();
+			});
 		},
-		callAjax: function(opts) {
+		openFeedbackForm:function() {
 
 		},
 		getAppLocation: function() {
-			return window.location.protocol + '//' + window.location.host;
+			return __w.location.protocol + '//' + __w.location.host;
+		},
+		getWidgetTemplate: function() {
+			var container = this.generateElement('div', [['class', 'ffw-container']]);
+			var ffwBtn = this.generateElement('a', [['class', 'ffw-btn'], ['href', 'javascript:void(0);']]);
+			ffwBtn.appendChild(this.generateElement('i', [['class', 'fa fa-info fw']]));
+			container.appendChild(ffwBtn);
+			var ffwOps = this.generateElement('ul', [['class', 'ffw-ops']]);
+			var ffwOp = this.generateElement('li', [['class', 'ffw-op']]);
+			var ffwOpBtn = this.generateElement('a', [['href', 'javascript:void(0);']]);
+			var ffwOpBtnTooltip = this.generateElement('span', [['class', 'ffw-op-btn-tooltip']]);
+			
+			var ffwOp1 = ffwOp.cloneNode(false);
+			var ffwOp1Btn = ffwOpBtn.cloneNode(false);
+			ffwOp1Btn.setAttribute('class', 'ffw-op-btn green ffw-contact');
+			ffwOp1Btn.appendChild(this.generateElement('i', [['class', 'fa fa-user fw']]));
+			var ffwOp1BtnTooltip = ffwOpBtnTooltip.cloneNode(false);
+			ffwOp1BtnTooltip.innerHTML = 'Contact Us';
+			ffwOp1.appendChild(ffwOp1Btn).appendChild(ffwOp1BtnTooltip);
+			ffwOps.appendChild(ffwOp1);
+
+			var ffwOp2 = ffwOp.cloneNode(false);
+			var ffwOp2Btn = ffwOpBtn.cloneNode(false);
+			ffwOp2Btn.setAttribute('class', 'ffw-op-btn red ffw-report');
+			ffwOp2Btn.appendChild(this.generateElement('i', [['class', 'fa fa-telegram fw']]));
+			var ffwOp2BtnTooltip = ffwOpBtnTooltip.cloneNode(false);
+			ffwOp2BtnTooltip.innerHTML = 'Report Problems';
+			ffwOp2.appendChild(ffwOp2Btn).appendChild(ffwOp2BtnTooltip);
+			ffwOps.appendChild(ffwOp2);
+			
+			var ffwOp3 = ffwOp.cloneNode(false);
+			var ffwOp3Btn = ffwOpBtn.cloneNode(false);
+			ffwOp3Btn.setAttribute('class', 'ffw-op-btn blue ffw-comments');
+			ffwOp3Btn.appendChild(this.generateElement('i', [['class', 'fa fa-comments fw']]));
+			var ffwOp3BtnTooltip = ffwOpBtnTooltip.cloneNode(false);
+			ffwOp3BtnTooltip.innerHTML = 'Share Feedback';
+			ffwOp3.appendChild(ffwOp3Btn).appendChild(ffwOp3BtnTooltip);
+			ffwOps.appendChild(ffwOp3);
+			container.appendChild(ffwOps);
+			return container;
+		},
+		generateElement: function(tagName, attrs) {
+			var elem;
+			if(tagName) {
+				elem = __d.createElement(tagName);
+				attrs.forEach(function(attrArr) {
+					var attrName = attrArr.length > 0 ? attrArr[0] : '';
+					var attrValue = attrArr.length > 1 ? attrArr[1] : '';
+					if(attrName) {
+						elem.setAttribute(attrName, attrValue);
+					}
+				}, this);
+			}
+			return elem;
+		},
+		getJSONToURLEncodedString: function(object) {
+			var encodedString = '';
+			for (var prop in object) {
+				if (object.hasOwnProperty(prop)) {
+					if (encodedString.length > 0) {
+						encodedString += '&';
+					}
+					encodedString += encodeURI(prop + '=' + object[prop]);
+				}
+			}
+			return encodedString;
+		},
+		callAjax: function(opts) {
+			var method = opts.method || 'GET';
+			var url = opts.url;
+			var sCb = opts.success || this.noop;
+			var eCb = opts.error || this.noop;
+			var params = opts.params || null;
+			var contentType = opts.contentType || 'application/x-www-form-urlencoded';
+			if(contentType === 'application/json' && params && typeof params !== 'string') {
+				params = JSON.stringify(params);
+			}
+			var xhr = new XMLHttpRequest();
+			xhr.open(method, url, true);
+			//xhr.withCredentials = true;// for CORS
+			xhr.setRequestHeader('Content-Type', contentType);
+			xhr.onreadystatechange = function() {
+				if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+					sCb(xhr.responseText);
+				} else {
+					eCb(xhr.responseText, xhr.status);
+				}
+			};
+			xhr.send(params);
+			return xhr;
+		},
+		showModalWindow: function() {
+			__d.querySelector('.ffw-modal').style.display = 'block';
+		},
+		hideModalWindow: function() {
+			__d.querySelector('.ffw-modal').style.display = 'none';
 		}
 	};
-	var options = {};
-	floatWidget.init(options);
+	ffWidget.init();
 }());
